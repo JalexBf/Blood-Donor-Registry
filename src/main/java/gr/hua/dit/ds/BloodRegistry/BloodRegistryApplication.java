362dc462d40bd44c6b1e84e50963cea4cbaddaca
JAlexBf <it218142@hua.gr>
2023-12-27 12:16:52 +0200

    first commit

---
+++ src/main/java/gr/hua/dit/ds/BloodRegistry/BloodRegistryApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.BloodRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/hua/dit/ds/BloodRegistry/BloodRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.BloodRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
